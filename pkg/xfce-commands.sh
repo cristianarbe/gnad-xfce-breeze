ee90acf218926f6b2878de1003419c1b622efccf
cristianarbe <c.ariza@posteo.net>
2019-12-14 15:48:10 +0000

    Add change wallpaper and change panel

--- pkg/xfce-commands.sh
+++ pkg/xfce-commands.sh
@@ -13,10 +13,31 @@ changeicon() {
 }
 
 changepanel() {
-  cp "$1" ~/.config/xfce4/xfconf/xfce-perchannel-xml
+  pkill xfconfd
+  pkill xfce4-panel
+  cp "$1" ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-panel.xml
+  cp "$2" ~/.config/xfce4/panel/whiskermenu-1.rc
+  xfce4-panel >/dev/null 2>&1 &
+  /usr/lib/x86_64-linux-gnu/xfce4/xfconf/xfconfd >/dev/null 2>&1 &
 }
 
 changefont() {
   xfconf-query -c xfwm4 -p /general/title_font -s "$1"
   xfconf-query -c xsettings -p /Gtk/FontName -s "$1"
+}
+
+changewallpaper() {
+  if test ! -d ~/.backgrounds; then
+    mkdir -p ~/.backgrounds
+  fi
+
+  cp "$1" ~/.backgrounds/wallpaper
+
+  property="$(xfconf-query -c xfce4-desktop -l | grep image-path | head -n 1)"
+  echo "$property"
+  xfconf-query -c xfce4-desktop -p "$property" -s ~/.backgrounds/wallpaper
+
+  property="$(xfconf-query -c xfce4-desktop -l | grep workspace | grep last-image | head -n 1)"
+  echo "$property"
+  xfconf-query -c xfce4-desktop -p "$property" -s ~/.backgrounds/wallpaper
 }
